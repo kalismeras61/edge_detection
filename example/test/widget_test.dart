d962db240b4a02f80a2a2ecfff6ad450beee6bf9
Yasin ilhan <yasinilhan61@gmail.com>
2019-09-03 20:14:31 +0300

    add tr lang

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -4,9 +4,4 @@
 // find child widgets in the widget tree, read text, and verify that the values of widget properties
 // are correct.
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:edge_detection_example/main.dart';
-
 void main() {}
